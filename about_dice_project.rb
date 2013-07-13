a631fe2094743ff86e78271cced587198ce34ec0
Cody Boppert <cboppert@buffalo.edu>
2013-07-12 20:41:19 -0400

    Complete to 266/281

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -5,6 +5,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # class DiceSet
 #   code ...
 # end
+class DiceSet
+  attr_accessor :values
+  def roll(numberOfDie)
+    prng = Random.new
+    @values = Array.new(numberOfDie)
+    for i in 0...numberOfDie
+        @values[i] = prng.rand(1..6)
+    end
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
